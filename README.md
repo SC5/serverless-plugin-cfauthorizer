a6df4e9649c7bf36977fe4e6c3efa8bb295ba1df
Kenneth Falck <kennu@iki.fi>
2016-10-26 21:34:56 +0300

    Moved custom authorizer config to the custom.cfAuthorizers section in serverless.yml

--- README.md
+++ README.md
@@ -12,23 +12,20 @@ authorizers, which are not yet supported by Serverless 1.0.
 
 ## Configuration
 
-You will first need to add a custom authorizer resource in the resources
+You will first need to add a custom authorizer in the custom cfAuthorizers
 section of your serverless.yml. Here is an example of a Cognito User Pool
-authorizer. To use this example, you need to substitute your own
-User Pool ARN on the last line.
+authorizer. To use this example, you need to substitute your own User Pool ARN
+on the last line. Note that the properties of the authorizer are standard
+CloudFormation properties, so you can use any supported values.
 
-    resources:
-      Resources:
+    custom:
+      cfAuthorizers:
         MyAuthorizer:
-          Type: "AWS::ApiGateway::Authorizer"
-          Properties:
-            Type: "COGNITO_USER_POOLS"
-            Name: "MyUserPoolAuthorizer"
-            IdentitySource: "method.request.header.Authorization"
-            RestApiId:
-              Ref: "ApiGatewayRestApi"
-            ProviderARNs:
-              - "arn:aws:cognito-idp:eu-west-1:xxxxxxxxxxxx:userpool/eu-west-1_xxxxxxxxx"
+          Type: "COGNITO_USER_POOLS"
+          Name: "MyUserPoolAuthorizer"
+          IdentitySource: "method.request.header.Authorization"
+          ProviderARNs:
+            - "arn:aws:cognito-idp:eu-west-1:xxxxxxxxxxxx:userpool/eu-west-1_xxxxxxxxx"
 
 Once the above resource has been added, you can configure individual HTTP
 endpoints in serverless.yml to use the authorizer. They will refer to it using
@@ -46,3 +43,6 @@ the resource name, which is MyAuthorizer in the example.
 After making the changes, all you need to do is redeploy the service:
 
     sls deploy
+
+Use API Gateway Console to verify that the authorizer has been deployed
+properly.
--- index.js
+++ index.js
@@ -27,23 +27,47 @@ class UserPoolPlugin {
       })
     })
 
-    // Scan all CloudFormation resources and apply custom authorizers.
     const resources = this.serverless.service.provider.compiledCloudFormationTemplate.Resources
+
+    // Check if the current compiled resource tree contains the API Gateway resource
+    let apiGatewayExists = false
     Object.keys(resources).map(resourceName => {
-      const resource = resources[resourceName]
-      if (resource.Type === 'AWS::ApiGateway::Method') {
-        const method = resource.Properties.HttpMethod.toUpperCase()
-        const path = this.getResourcePath(resources[resource.Properties.ResourceId.Ref], resources)
-        if (this.cfAuthorizers[path]) {
-          const cfAuthorizer = this.cfAuthorizers[path][method]
-          if (cfAuthorizer) {
-            this.serverless.cli.log('Adding CloudFormation Authorizer ' + cfAuthorizer + ' to ' + method + ' ' + path)
-            resource.Properties.AuthorizationType = 'COGNITO_USER_POOLS'
-            resource.Properties.AuthorizerId = {Ref:cfAuthorizer}
-          }
-        }
+      if (resourceName === 'ApiGatewayRestApi') {
+        apiGatewayExists = true
       }
     })
+
+    if (apiGatewayExists) {
+      // Scan all CloudFormation resources and apply custom authorizers.
+      Object.keys(resources).map(resourceName => {
+        const resource = resources[resourceName]
+        if (resource.Type === 'AWS::ApiGateway::Method') {
+          const method = resource.Properties.HttpMethod.toUpperCase()
+          const path = this.getResourcePath(resources[resource.Properties.ResourceId.Ref], resources)
+          if (this.cfAuthorizers[path]) {
+            const cfAuthorizer = this.cfAuthorizers[path][method]
+            if (cfAuthorizer) {
+              this.serverless.cli.log('Adding CloudFormation Authorizer ' + cfAuthorizer + ' to ' + method + ' ' + path)
+              resource.Properties.AuthorizationType = this.serverless.service.custom.cfAuthorizers[cfAuthorizer].Type
+              resource.Properties.AuthorizerId = {Ref:cfAuthorizer}
+            }
+          }
+        }
+      })
+
+      // Add custom authorizer resources to CloudFormation resources
+      const cfAuthorizers = this.serverless.service.custom.cfAuthorizers
+      Object.keys(cfAuthorizers).map(resourceName => {
+        const properties = cfAuthorizers[resourceName]
+        properties.RestApiId = {
+          Ref: 'ApiGatewayRestApi'
+        }
+        resources[resourceName] = {
+          Type: "AWS::ApiGateway::Authorizer",
+          Properties: properties,
+        }
+      })
+    }
   }
 
   // Get the full API Gateway resource path of a relative resource
